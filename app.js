cee198f4182f213175cc50f6a0448b732ccc4014
code_kareem <seniorteck4real@gmail.com>
2021-06-20 16:36:54 -0400

    fix in js

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ let password = document.querySelector("#password")
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
+
+    if(password.value.includes("@")){
+        console.log("it contains special character")
+    }
     console.log('user submit file')
 });
