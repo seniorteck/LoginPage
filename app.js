497b6dcb071440ca8302ddf5985a23d321a09859
code_kareem <seniorteck4real@gmail.com>
2021-06-20 14:17:51 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let form = document.querySelector(".form-group");
+
+let password = document.querySelector("#password")
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    console.log('user submit file')
+});
